2c4f89ac3a09583a2ae428426acde46fb24b7048
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-02-03 08:30:02 +0100

    feladat

---
+++ 01_feladat.py
@@ -0,0 +1,14 @@
+"""
+1. Feladat
+Írj egy programot, amely tartalmaz egy 'osszegzo' nevű függvényt, ami a paraméterként átvett
+listaelemeket (egész számokat) összeadja és az összegükkel tér vissza! A program tartalmazza a
+függvény hívását is!
+"""
+
+def osszegzo(listaelemek):
+    osszeg = 0
+    for elem in listaelemek:
+        osszeg += elem
+    return osszeg  
+
+print(osszegzo([1, 5, 9, 13, 29]))
---
+++ 02_feladat.py
@@ -0,0 +1,14 @@
+"""
+2. Feladat
+Írj egy programot, amely tartalmaz egy 'paros_e' nevű függvényt,
+amely True értékkel tér vissza, ha a paraméterként átvett listaelemek (egész számok) között van páros,
+ellenkező esetben a visszatérési érték False! A program tartalmazza a függvény hívását is!
+"""
+
+def paros_e(listaelemek):
+    for elem in listaelemek:
+        if elem % 2 != 0:
+            return False
+    return True
+
+print(paros_e([1, 8, 17, 22, 40]))
